608a27682c3cecedf15c09726ce1157fba64fefa
Udo Kohlmeyer <udo@vmare.com>
2023-02-09 10:18:55 +1100

    Update Copyright headers

--- spring-gemfire/src/test/resources/data.sql
+++ spring-gemfire/src/test/resources/data.sql
@@ -1,1 +1,6 @@
+/*
+ * Copyright (c) VMware, Inc. 2023. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 INSERT INTO customers (id, name) VALUES (16, 'Pie Doe');
--- spring-gemfire/src/test/resources/schema.sql
+++ spring-gemfire/src/test/resources/schema.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) VMware, Inc. 2023. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 CREATE TABLE IF NOT EXISTS customers (
 	id BIGINT PRIMARY KEY,
   	name VARCHAR(256) NOT NULL
