909a5bbf16bbe214990d835a22a01a4c50222f39
Udo Kohlmeyer <udo@vmare.com>
2023-03-31 21:43:57 +0200

    SDGF-79: Update dependency versions

--- build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
+++ build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
@@ -23,13 +23,13 @@ class DependencyConstraints {
     depVersionMapping.put("antlrVersion", "2.7.7")
     depVersionMapping.put("findbugsVersion", "3.0.2")
     depVersionMapping.put("multithreadedtcVersion", "1.01")
-    depVersionMapping.put("springBootVersion", "3.0.1")
+    depVersionMapping.put("springBootVersion", "3.0.5")
     depVersionMapping.put("springShellVersion", "1.2.0.RELEASE")
-    depVersionMapping.put("springFrameworkVersion", "6.0.4")
-    depVersionMapping.put("springSecurityVersion", "6.0.1")
+    depVersionMapping.put("springFrameworkVersion", "6.0.7")
+    depVersionMapping.put("springSecurityVersion", "6.0.2")
     depVersionMapping.put("testcontainersVersion", "1.17.6")
     depVersionMapping.put("gemfireVersion", "9.15.4-RC.3")
-    depVersionMapping.put("springTestGemFire", "1.0.0-build.11")
+    depVersionMapping.put("springTestGemFire", "1.0.0")
 
     return depVersionMapping
   }
