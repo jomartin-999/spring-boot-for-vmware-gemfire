25b19b27a46efcb64040eba85ebc858257979857
Patrick Johnson <jpatrick@vmware.com>
2023-04-05 12:18:01 -0700

    SDGF-80: Port spring-geode-samples to spring-gemfire-samples

--- spring-geode-samples/intro/getting-started/src/main/resources/geode/bin/start-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# Copyright (c) VMware, Inc. 2023. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-
-gfsh -e "run --file=@samples-dir@/intro/getting-started/src/main/resources/geode/bin/start-cluster.gfsh"
